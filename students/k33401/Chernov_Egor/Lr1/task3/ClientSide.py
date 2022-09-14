68ce5e97a120feb86d6e3acbbfd9035d35388afe
EgorChernov3140 <egorchernov3140@gmail.com>
2022-09-14 11:36:12 +0300

    Task 3 was refactored

--- students/k33401/Chernov_Egor/Lr1/task3/ClientSide.py
+++ students/k33401/Chernov_Egor/Lr1/task3/ClientSide.py
@@ -4,6 +4,7 @@ import socket
 with socket.socket() as s:
     s.connect(('127.0.0.1', 8080))
     s.settimeout(5)
+    s.send(b"GET / HTTP/1.1\n")
     data = s.recv(16384)
     decodeData = data.decode('utf-8')
     print(decodeData)
--- students/k33401/Chernov_Egor/Lr1/task3/ServerSide.py
+++ students/k33401/Chernov_Egor/Lr1/task3/ServerSide.py
@@ -6,7 +6,8 @@ with socket.socket() as s:
     s.listen(1)
     conn, addr = s.accept()
     with conn:
-        with open('res/index.html') as fin:
-            massage = fin.read()
-        response = 'HTTP/1.0 200 OK\n\n' + massage
+        with open('C:/Users/krish/Учеба/5 семестр/Web-разработка/ITMO_ICT_WebDevelopment_2022-2023/students/k33401/Chernov_Egor/Lr1/res/index.html') as fin:
+            message = fin.read()
+        print(conn.recv(16348).decode('utf-8'))
+        response = 'HTTP/1.0 200 OK\n\n' + message
         conn.sendall(response.encode('utf-8'))
