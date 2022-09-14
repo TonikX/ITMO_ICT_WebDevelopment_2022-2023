6892f16625ccaab2d2b29b3f91b357044e17f3df
Ann <AnnPetrosian>
2022-09-14 22:00:31 +0300

    add client

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
@@ -1,13 +1,16 @@
 import socket
-
-port = 14900
-host = "127.0.0.1"
-
+ 
+port = 631
+host = socket.gethostbyname("localhost")
+mess="Hello, server"
+message = bytes(mess, 'utf-8')
+#host="127.0.0.1"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+ 
 sock.connect((host, port))
-sock.send('Hello, server')
-
+sock.send(message)
+ 
 data = sock.recv(1024)
-print (data)
+print(str(message)[2:-1])
+#print (message)
 sock.close()
