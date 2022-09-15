1abea717f3d9c0f2a79c7fb539b827a5d4b3dc9f
EgorChernov3140 <egorchernov3140@gmail.com>
2022-09-15 15:41:07 +0300

    Task 4: Server is ready, but we should to refactor in_data()

--- students/k33401/Chernov_Egor/Lr1/task4/ServerSide.py
+++ students/k33401/Chernov_Egor/Lr1/task4/ServerSide.py
@@ -1,39 +1,49 @@
-import concurrent.futures
-import queue
 import socket
 import threading
 
 
-def connect_user(sock):
-    conn = sock.accept()[0]
-    with conn:
-        conn.recv(16384)
+def in_data(t_conn):
+    t_user = str(t_conn.getpeername()[1])
+    while 1:
+        try:
+            data = t_conn.recv(16384)
+            decode_data = data.decode('utf-8')
+            if decode_data == '/exit':
+                users.remove(t_conn)
+                for user in users:
+                    user.send(f"...User {t_user} left...\n".encode('utf-8'))
+                t_conn.close()
+                if len(users) == 0:
+                    s.close()
+                break
+            for user in users:
+                if user == t_conn:
+                    continue
+                user.send(('User ' + t_user + ': ' + decode_data).encode('utf-8'))
+        except socket.error:
+            print("Error in in_data()")
+            print(len(users))
+            break
 
 
-def producer(queue, event):
-    """We're saving a message in a queue."""
-    while not event.is_set():
-        message = ""
-        queue.put(message)
-
-
-def consumer(queue, event):
-    """We're getting a message from the queue and send to a chat."""
-    while not event.is_set() or not pipeline.empty():
-        message = queue.get()
-
-
-with socket.socket() as s:
-    s.bind(('', 8080))
-    s.listen(4)
-
-    pipeline = queue.Queue(10)
-    event = threading.Event()
-    with concurrent.futures.ThreadPoolExecutor(4) as executor:
-        executor.submit(connect_user, s)
-        # executor.submit(producer, pipeline, event)
-        # executor.submit(consumer, pipeline, event)
-
-        # time.sleep(0.1)
-
-        # event.set()
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        host = ''
+        port = 8080
+        s.bind((host, port))
+        s.listen(5)
+        users = []
+        print('Chat\'s started')
+        while 1:
+            try:
+                conn, addr = s.accept()
+                conn.send(b"...Write \"/exit\" for live this chat...\n")
+                message = f"...New member: {addr[1]}...\n"
+                for user in users:
+                    user.send(message.encode('utf-8'))
+                users.append(conn)
+                t = threading.Thread(target=in_data, name='in', args=(conn,))
+                t.start()
+            except socket.error:
+                print('Chat\' stopped')
+                break
