03a517d63b9323d3b3f29994ab789cdea072017a
AnnPetrosian <anyapet6020@gmail.com>
2022-09-16 23:59:24 +0300

    port and message - decode - fixed

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
@@ -1,26 +1,16 @@
 import socket
-
+ 
 port = 3968
-
 host = socket.gethostbyname("localhost")
-
 mess="Hello, server"
 message = bytes(mess, 'utf-8')
-
 #host="127.0.0.1"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #_stream for tcp dgram udp
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+ 
 sock.connect((host, port))
 sock.send(message)
  
 data = sock.recv(1024)
-
 print(data.decode())
-
-
-print(str(message)[2:-1])
 #print (message)
-
 sock.close()
