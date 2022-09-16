3869e6e90e9538a2326cf0e9c2feccf137ef2406
AnnPetrosian <anyapet6020@gmail.com>
2022-09-16 23:34:26 +0300

    windows port fixed message mixed up

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_1/client.py
@@ -1,13 +1,16 @@
 import socket
-
-port = 14900
-host = "127.0.0.1"
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+ 
+port = 3968
+host = socket.gethostbyname("localhost")
+mess="Hello, server"
+message = bytes(mess, 'utf-8')
+#host="127.0.0.1"
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #_stream for tcp dgram udp
+ 
 sock.connect((host, port))
-sock.send('Hello, server')
-
+sock.send(message)
+ 
 data = sock.recv(1024)
-print (data)
+print(data.decode())
+#print (message)
 sock.close()
