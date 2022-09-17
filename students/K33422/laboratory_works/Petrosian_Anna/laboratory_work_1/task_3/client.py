20b3a200e299078059ea3bf8ec73f09e663d3cbc
AnnPetrosian <anyapet6020@gmail.com>
2022-09-17 20:22:00 +0300

    first

---
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_3/client.py
@@ -0,0 +1,16 @@
+import socket
+ 
+port = 3968
+host = socket.gethostbyname("localhost")
+"""mess="Hello, server"
+message = bytes(mess, 'utf-8')"""
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #_stream for tcp dgram udp
+ 
+sock.connect((host, port))
+sock.send(message)
+ 
+data = sock.recv(1024)
+print(data.decode())
+#print (message)
+sock.close()
