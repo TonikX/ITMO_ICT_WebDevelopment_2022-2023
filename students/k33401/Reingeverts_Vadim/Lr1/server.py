1a433331d442ed292ccb793a72f08f8b2c7de7ef
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-09-19 10:31:43 +0300

    feat: make it possible to Ctrl + C out of the server

--- students/k33401/Reingeverts_Vadim/Lr1/server.py
+++ students/k33401/Reingeverts_Vadim/Lr1/server.py
@@ -2,17 +2,29 @@ import socket
 
 # UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Ensures that port is always ready to be used again
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('localhost', 12346))
 
+# Makes keyboard interrupt possible at all times
+sock.settimeout(1.0)
+
 while True:
     try:
-        data, client_address = sock.recvfrom(2048)
-        data = data.decode('utf-8')
+        connection, client_address = None, None
+        try:
+            connection, client_address = sock.recvfrom(2048)
+        except IOError:
+            continue
+
+        data = connection.decode('utf-8')
         print('Recived:', data)
 
         sock.sendto(b"Hello, client", client_address)
+
     except KeyboardInterrupt:
         print("Stopping server...")
-        sock.close()
+        if connection:
+            connection.close()
         break
+sock.close()
