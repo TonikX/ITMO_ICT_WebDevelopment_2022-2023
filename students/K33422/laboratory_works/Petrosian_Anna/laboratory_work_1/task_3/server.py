f4a8b75962504fbb13de59b0e7eea734cb01c79a
AnnPetrosian <anyapet6020@gmail.com>
2022-09-20 13:50:09 +0300

    open index

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_3/server.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_3/server.py
@@ -1,29 +1,20 @@
 import socket
-#import urllib
-import codecs
+
 port = 3968
 host = socket.gethostbyname("localhost")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) #use TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+sock.bind((host, port))
 
-"""page = urllib.urlopen("index.html").read()
-page = urllib.request.urlopen("/path/").read() 
-print(page) 
-page_content = page.read()
-print(page_content)
+sock.listen(10)
+sock, addr = sock.accept()
+sock.recv(16384)
 
-mess="Hello, client"
-message = bytes(mess, 'utf-8')"""
-
-f = codecs.open("index.html", 'r', 'utf-8')
-pg=f.read()
-
-
-sock.bind((host, port)) #связь сокета с хостом и портом
-sock.listen(10) #n listenings 
-cl_sock, addr = sock.accept() #приём и посылка данных
-data = cl_sock.recv(1024) #порция данных
-print(data.decode())
-cl_sock.sendall(pg) #page_content
-
-sock.close() #закрыли соединени
+response_type = "HTTP/1.0 200 OK\n"
+headers = "Content-Type: text/html\n\n"
+page = open('index.html','r')
+body = page.read()
+resp = response_type + headers + body
+sock.send(resp.encode("utf-8"))
+page.close()
+sock.close()
