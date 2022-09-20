c3a87f0798bbfb1dcc57d4282acf5e69749ce47d
AnnPetrosian <anyapet6020@gmail.com>
2022-09-20 13:51:03 +0300

    empty message unsuitable

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_3/client.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_3/client.py
@@ -1,16 +1,15 @@
 import socket
-
+ 
 port = 3968
 host = socket.gethostbyname("localhost")
-"""mess="Hello, server"
-message = bytes(mess, 'utf-8')"""
-
+mess="Hello, server"
+message = bytes(mess, 'utf-8')
+#host="127.0.0.1"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #_stream for tcp dgram udp
-
+ 
 sock.connect((host, port))
-
-sock.sendall(b'')
-
+sock.send(message)
+ 
 data = sock.recv(1024)
 print(data.decode())
 #print (message)
