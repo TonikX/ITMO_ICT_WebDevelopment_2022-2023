359e8d0b22ec36410b19969f93f7ad135263de83
EgorChernov3140 <egorchernov3140@gmail.com>
2022-09-20 18:46:26 +0300

    Request was refactored

--- students/k33401/Chernov_Egor/Lr1/task5/Request.py
+++ students/k33401/Chernov_Egor/Lr1/task5/Request.py
@@ -1,7 +1,3 @@
-from functools import lru_cache
-from urllib.parse import parse_qs, urlparse
-
-
 class Request:
     def __init__(self, method, target, version, headers, rfile):
         self.method = method
@@ -9,20 +5,6 @@ class Request:
         self.version = version
         self.headers = headers
         self.rfile = rfile
-
-    @property
-    def path(self):
-        return self.url.path
-
-    @property
-    @lru_cache(maxsize=None)
-    def query(self):
-        return parse_qs(self.url.query)
-
-    @property
-    @lru_cache(maxsize=None)
-    def url(self):
-        return urlparse(self.target)
 
     def body(self):
         size = self.headers.get('Content-Length')
