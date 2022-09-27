1a13c4c9002de22a98f2059802115468bf3fcede
AnnPetrosian <anyapet6020@gmail.com>
2022-09-28 02:41:04 +0300

    exit fixed

--- students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_4/server.py
+++ students/K33422/laboratory_works/Petrosian_Anna/laboratory_work_1/task_4/server.py
@@ -5,47 +5,62 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 3968))
 sock.listen()
 
-clients = []
+interlocuteurs = []
 usernames = []
 
 
 def broadcast(message):
-    for interlocuteur in clients:
-        interlocuteur.send(message)
+    for sock_int in interlocuteurs:
+        sock_int.send(message)
 
 
-def handle(interlocuteur):
+def handle(sock_int):
     while True:
-        message = interlocuteur.recv(4096)
-        #broadcast(message)
+        try:
+            message = sock_int.recv(4096)
 
-        if message == b"EXIT":
-            interlocuteur.connection.close()
-            clients.remove(interlocuteur)
-            broadcast(f'{username} left'.encode('utf-8'))
+            if "exit" in message.decode('utf-8'):
+                exitclient(sock_int)
+                break
+
+            broadcast(message)
+
+        except Exception as e:
+            exitclient(sock_int)
             break
-        else:
-                broadcast(message)
+
+
+def exitclient(sock_int):
+    index = interlocuteurs.index(sock_int)
+    interlocuteurs.remove(sock_int)
+    sock_int.close()
+    username = usernames[index]
+    broadcast(f'bye, {username}'.encode('utf-8'))
+    usernames.remove(username)
 
 
 def receive():
     while True:
         try:
-            interlocuteur, client_address = sock.accept()
-            print(f'accepted connection from {client_address}')
+            sock_int, client_address = sock.accept()
+            print(f'connection established {client_address}')
 
-            interlocuteur.send('NICKNAME'.encode('utf-8'))
-            username = interlocuteur.recv(4096).decode('utf-8')
-            clients.append(interlocuteur)
+            sock_int.send('NICKNAME'.encode('utf-8'))
+            username = sock_int.recv(4096).decode('utf-8')
+            interlocuteurs.append(sock_int)
             usernames.append(username)
             broadcast(f'{username} joined'.encode('utf-8'))
 
-            handle_thread = threading.Thread(target=handle, args=(interlocuteur,))
+            handle_thread = threading.Thread(target=handle, args=(sock_int,))
             handle_thread.start()
+
+        except KeyboardInterrupt:
+            print ("server closed")
+            sock.close()
+            break
 
         except Exception as e:
             print('Exception:', e)
             broadcast(f'')
-
 
 receive()
