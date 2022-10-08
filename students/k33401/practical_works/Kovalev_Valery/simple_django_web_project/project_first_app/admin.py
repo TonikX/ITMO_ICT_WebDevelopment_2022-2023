2fdb534abb29e950ba2ba170614143c3ef99bf2d
kovalev-vxx <kovalev.vxx@gmail.com>
2022-10-08 12:05:46 +0300

    Сделал модель добавил в базу данные

--- students/k33401/practical_works/Kovalev_Valery/simple_django_web_project/project_first_app/admin.py
+++ students/k33401/practical_works/Kovalev_Valery/simple_django_web_project/project_first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from project_first_app.models import *
 
-# Register your models here.
+admin.site.register(Car)
+admin.site.register(Own)
+admin.site.register(CarOwner)
+admin.site.register(License)
