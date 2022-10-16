27d7f335053c6fbd6f93fd9abeb78ac643e163ee
Nikolas <timofeevnik41@gmail.com>
2022-10-17 00:04:02 +0300

    fix(socket): fixed lr3 lr4

--- students/k33402/Timofeev_Nikolay/LR1/src/socket_chat_server/client.py
+++ students/k33402/Timofeev_Nikolay/LR1/src/socket_chat_server/client.py
@@ -16,36 +16,36 @@ logger.add(sys.stdout, format=logger_format)
 
 class ChatClient:
     def __init__(self, ip: str, port: int):
-        self.alias = f"Anonymous_{uuid.uuid1().hex[:5]}"
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((ip, port))
+        self._alias = f"Anonymous_{uuid.uuid1().hex[:5]}"
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.connect((ip, port))
 
-    def receive(self):
+    def _receive(self):
         while True:
             try:
-                message = self.sock.recv(1024).decode()
+                message = self._sock.recv(1024).decode()
                 logger.info(message)
             except Exception as e:
                 logger.info("Disconnected from server")
                 break
-        self.sock.close()
+        self._sock.close()
 
-    def send(self):
+    def _send(self):
         while True:
             message = input()
             if message == '/quit':
                 break
-            self.sock.send(message.encode())
-        self.sock.close()
+            self._sock.send(message.encode())
+        self._sock.close()
 
     def start(self):
         try:
-            self.sock.send(self.alias.encode())
+            self._sock.send(self._alias.encode())
 
-            receive_thread = threading.Thread(target=self.receive)
+            receive_thread = threading.Thread(target=self._receive)
             receive_thread.start()
 
-            send_thread = threading.Thread(target=self.send)
+            send_thread = threading.Thread(target=self._send)
             send_thread.start()
         except Exception:
             logger.warning("Something went wrong")
