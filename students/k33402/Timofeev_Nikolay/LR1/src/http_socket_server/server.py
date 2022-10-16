18ccde3c12dac549b6cde2cb53afa30f738c6e9d
Nikolas <timofeevnik41@gmail.com>
2022-10-17 00:03:46 +0300

    fix(socket): fixed lr3 lr4

--- students/k33402/Timofeev_Nikolay/LR1/src/http_socket_server/server.py
+++ students/k33402/Timofeev_Nikolay/LR1/src/http_socket_server/server.py
@@ -1,188 +1,40 @@
 import socket
-import sys
-from email.parser import Parser
-from functools import lru_cache
-from urllib.parse import parse_qs, urlparse
-
-MAX_LINE = 64 * 1024
-MAX_HEADERS = 100
 
 
-class MyHTTPServer:
-    def __init__(self, host: str, port: int, server_name: str):
-        self._host = host
-        self._port = port
-        self._server_name = server_name
-        self._data: dict[str, list[str]] = {}
+class HTTPServer:
+    def __init__(self, host: str, port: int, name: str) -> None:
+        self._host, self._port = host, port
+        self._name: str = name
+        self._server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def serve_forever(self):
-        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-
+    def serve_forever(self) -> None:
         try:
-            serv_sock.bind((self._host, self._port))
-            serv_sock.listen()
-
+            self._server.bind((self._host, self._port))
+            self._server.listen()
             while True:
-                clientsocket, _ = serv_sock.accept()
-                try:
-                    self.serve_client(clientsocket)
-                except Exception as e:
-                    print('Client serving failed:', e)
-        finally:
-            serv_sock.close()
+                client, address = self._server.accept()
+                self._serve_client(client)
+        except KeyboardInterrupt:
+            self._server.close()
 
-    def serve_client(self, clientsocket):
-        try:
-            req = self.parse_request(clientsocket)
-            resp = self.handle_request(req)
-            self.send_response(clientsocket, resp)
-        except ConnectionResetError:
-            clientsocket = None
-        except Exception as e:
-            print('Client serving failed:', e)
+    def _serve_client(self, client) -> None:
+        html = self._handle_request()
+        self._send_response(client, html)
 
-        if clientsocket:
-            clientsocket.close()
+    @staticmethod
+    def _handle_request(filename: str = 'index.html') -> str:
+        with open(filename, "r") as file:
+            body = file.read()
+        return body
 
-    def parse_request(self, clientsocket):
-        rfile = clientsocket.makefile('rb')
-        raw = rfile.readline(MAX_LINE + 1)
-
-        if len(raw) > MAX_LINE:
-            raise Exception('Request line is too long')
-
-        req_line = str(raw, 'iso-8859-1')
-        req_line = req_line.rstrip('\r\n')
-        words = req_line.split()
-        if len(words) != 3:
-            raise Exception('Malformed request line')
-
-        method, target, ver = words
-
-        if ver != 'HTTP/1.1':
-            raise Exception('Unexpected HTTP version')
-
-        headers = self.parse_headers(rfile)
-        host = headers.get('Host')
-        if not host:
-            raise Exception('Bad request')
-        if host not in (self._server_name, f'{self._server_name}:{self._port}'):
-            raise Exception('Not found')
-
-        return Request(method, target, ver, headers, rfile)
-
-    def parse_headers(self, rfile):
-        headers = []
-        while True:
-            line = rfile.readline(MAX_LINE + 1)
-            if len(line) > MAX_LINE:
-                raise Exception('Header line is too long')
-
-            if line in (b'\r\n', b'\n', b''):
-                break
-
-            headers.append(line)
-            if len(headers) > MAX_LINE:
-                raise Exception('Too many headers')
-        sheaders = b''.join(headers).decode('iso-8859-1')
-
-        return Parser().parsestr(sheaders)
-
-    def handle_request(self, req):
-        if req.path == '/rating' and req.method == 'POST':
-            return self.handle_post_grades(req)
-
-        if req.path.startswith('/rating/') and req.method == 'GET':
-            subject_name = req.path[len('/rating/'):]
-            return self.handle_get_rating(req, subject_name)
-
-        raise Exception('Request not found')
-
-    def handle_post_grades(self, req):
-        subject_name = req.query['subject_name'][0]
-        grade = req.query['grade'][0]
-        if subject_name in self._data.keys():
-            self._data[subject_name].append(grade)
-        else:
-            new_subject = []
-            new_subject.append(grade)
-            self._data[subject_name] = new_subject
-
-        return Response(204, 'Created')
-
-    def handle_get_rating(self, req, subject_name):
-        if subject_name not in self._data.keys():
-            raise Exception('Request not found')
-
-        content_type = 'text/html; charset=utf-8'
-        body = '<html><head></head><body>'
-        ans = f'{subject_name}: '
-
-        for grade in self._data[subject_name]:
-            ans += grade + ' '
-
-        body += f'<div>{ans}</div>'
-        body += '</body></html>'
-        body = body.encode('utf-8')
-        headers = [('Content-Type', content_type), ('Content-Length', len(body))]
-        return Response(200, 'OK', headers, body)
-
-    def send_response(self, clientsocket, response):
-        wfile = clientsocket.makefile('wb')
-        status_line = f'HTTP/1.1 {response.status} {response.reason}\r\n'
-        wfile.write(status_line.encode('iso-8859-1'))
-
-        if response.headers:
-            for (key, value) in response.headers:
-                header_line = f'{key}: {value}\r\n'
-                wfile.write(header_line.encode('iso-8859-1'))
-
-        wfile.write(b'\r\n')
-
-        if response.body:
-            wfile.write(response.body)
-
-        wfile.flush()
-        wfile.close()
-
-
-class Request:
-    def __init__(self, method, target, version, headers, rfile):
-        self.method = method
-        self.target = target
-        self.version = version
-        self.headers = headers
-        self.rfile = rfile
-
-    @property
-    def path(self):
-        return self.url.path
-
-    @property
-    @lru_cache(maxsize=None)
-    def query(self):
-        return parse_qs(self.url.query)
-
-    @property
-    @lru_cache(maxsize=None)
-    def url(self):
-        return urlparse(self.target)
-
-
-class Response:
-    def __init__(self, status, reason, headers=None, body=None):
-        self.status = status
-        self.reason = reason
-        self.headers = headers
-        self.body = body
+    @staticmethod
+    def _send_response(client: socket.socket, document: str) -> None:
+        client.sendall(
+            f'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{document}'.encode()
+        )
 
 
 if __name__ == '__main__':
-    host = "0.0.0.0"
-    port = 8888
-    name = "index.html"
-    server = MyHTTPServer(host, port, name)
-
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
+    host = ('127.0.0.1', 2000)
+    print(f'server started at http://{host[0]}:{host[1]}')
+    HTTPServer(*host, 'example.com').serve_forever()
--- students/k33402/Timofeev_Nikolay/LR1/src/socket_chat_server/server.py
+++ students/k33402/Timofeev_Nikolay/LR1/src/socket_chat_server/server.py
@@ -5,46 +5,46 @@ from loguru import logger
 
 class ChatServer:
     def __init__(self, ip: str, host: int):
-        self.sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((ip, host))
-        self.sock.listen()
-        self.clients: dict[socket.socket, str] = {}
+        self._sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.bind((ip, host))
+        self._sock.listen()
+        self._clients: dict[socket.socket, str] = {}
 
-    def broadcast(self, message: str):
-        for client in self.clients.keys():
+    def _broadcast(self, message: str):
+        for client in self._clients.keys():
             client.send(message.encode())
 
-    def handle_client(self, client: socket.socket):
+    def _handle_client(self, client: socket.socket):
         while True:
             try:
                 message = client.recv(1024).decode()
                 if message == '/quit' or not message:
                     client.close()
                 else:
-                    self.broadcast(f"{self.clients[client]}: {message}")
+                    self._broadcast(f"{self._clients[client]}: {message}")
             except Exception as e:
-                msg = f'{self.clients[client]} left chat'
+                msg = f'{self._clients[client]} left chat'
                 client.close()
-                self.clients.pop(client)
-                self.broadcast(msg)
+                self._clients.pop(client)
+                self._broadcast(msg)
                 logger.info(msg)
                 break
 
-    def receive(self):
-        logger.info(f"Server working on {self.sock.getsockname()}")
+    def _receive(self):
+        logger.info(f"Server working on {self._sock.getsockname()}")
 
         while True:
-            client, address = self.sock.accept()
+            client, address = self._sock.accept()
             logger.info(f"{str(address)} connected!")
             alias = client.recv(1024).decode()
-            self.clients[client] = alias
-            self.broadcast(f"{alias} connected. Send /quit to disconnect")
-            thread = threading.Thread(target=self.handle_client, args=(client,))
+            self._clients[client] = alias
+            self._broadcast(f"{alias} connected. Send /quit to disconnect")
+            thread = threading.Thread(target=self._handle_client, args=(client,))
             thread.start()
 
     def run(self):
         try:
-            self.receive()
+            self._receive()
         except KeyboardInterrupt:
             logger.critical("Server stopped by sigterm")
 
