29409e9e2edf53a51a19426308382fa2706c7468
EgorChernov3140 <egorchernov3140@gmail.com>
2022-10-18 12:43:04 +0300

    Change location Lr1

--- students/k33401/Chernov_Egor/Lr1/task5/HTTPError.py
+++
@@ -1,6 +0,0 @@
-class HTTPError(Exception):
-    def __init__(self, status, reason, body=None):
-        super()
-        self.status = status
-        self.reason = reason
-        self.body = body
