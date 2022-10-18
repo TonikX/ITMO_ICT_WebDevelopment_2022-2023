d64103b3ed4d882d104f4f62905101e9d8c60778
EgorChernov3140 <egorchernov3140@gmail.com>
2022-10-18 16:06:44 +0300

    Create tables and make migrate

--- students/k33401/practical_works/Chernov_Egor/simple_django_web_project/django_project_cars/cars_first_app/models.py
+++ students/k33401/practical_works/Chernov_Egor/simple_django_web_project/django_project_cars/cars_first_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+
+
+class DriverLicense(models.Model):
+    id_driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+    issue_date = models.DateField(null=False, blank=False)
+
+
+class Car(models.Model):
+    license_plate = models.CharField(max_length=15)
+    car_brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=30, null=True, blank=True)
+
+
+class Ownership(models.Model):
+    id_driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    id_car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateField(null=False, blank=False)
+    end_date = models.DateField()
