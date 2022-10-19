96a17836a8d3fc34c3a416c9d843de368058d177
EgorChernov3140 <egorchernov3140@gmail.com>
2022-10-19 14:38:57 +0300

    Implement a method for getting info about the driver

--- students/k33401/practical_works/Chernov_Egor/simple_django_web_project/django_project_cars/cars_first_app/views.py
+++ students/k33401/practical_works/Chernov_Egor/simple_django_web_project/django_project_cars/cars_first_app/views.py
@@ -1,3 +1,16 @@
+# from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
+from .models import Driver
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def get_driver(request, id):
+    try:
+        d = Driver.objects.get(pk=id)
+    except Driver.DoesNotExist:
+        raise Http404("Poll does not exist")
+    return render(request, 'driver.html', {'driver': d})
