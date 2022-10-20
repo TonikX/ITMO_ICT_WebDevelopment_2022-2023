a60e8d347ac2106d48c90c05782c2a22063ef981
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2022-10-20 18:59:48 +0300

    :hammer: [fix] config and functions naming

--- students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/urls.py
+++ students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('owner/<int:owner_id>/', views.detail),
+    path('owner/<int:owner_id>/', views.get_owner),
 ]
--- students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/views.py
+++ students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from practical_work_1.models import Car, Owner, Ownership, DriverLicense
 
 
-def detail(request, owner_id):
+def get_owner(request, owner_id):
     try:
         owner = Owner.objects.get(pk=owner_id)
     except Owner.DoesNotExist:
-        raise Http404("Car does not exist")
+        raise Http404("Owner does not exist")
     return render(
         request, 'owner.html', {'owner': owner}
     )
