cae90a64fc04d4650f221b538dccd981389151b7
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2022-10-21 21:00:34 +0300

    :hammer: [fix] many to many

--- students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/models.py
+++ students/k33401/Kobelev_Lev/Practical_Works/simple_django_web_project/practical_work_1/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 
+class Car(models.Model):
+    state_number = models.CharField(max_length=15)
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.brand, self.model)
+
+    @classmethod
+    def as_view(cls):
+        pass
+
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     birthday = models.DateField(null=True, blank=True)
+    cars = models.ManyToManyField(Car, through='Ownership')
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.second_name)
@@ -19,20 +34,6 @@ class DriverLicense(models.Model):
     license_number = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
     issue_date = models.DateField()
-
-
-class Car(models.Model):
-    state_number = models.CharField(max_length=15)
-    brand = models.CharField(max_length=20)
-    model = models.CharField(max_length=20)
-    color = models.CharField(max_length=30, null=True, blank=True)
-
-    def __str__(self):
-        return '{} {}'.format(self.brand, self.model)
-
-    @classmethod
-    def as_view(cls):
-        pass
 
 
 class Ownership(models.Model):
