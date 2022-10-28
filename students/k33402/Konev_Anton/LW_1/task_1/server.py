6d43af30012c01b110c534c5ac616c985112af59
VenomouSTaCo <79048300+VenomouSTaCo@users.noreply.github.com>
2022-10-28 21:28:37 +0300

    Delete students/k33402/Konev_Anton/LW_1 directory

--- students/k33402/Konev_Anton/LW_1/task_1/server.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("127.0.0.1", 8071))
-sock.listen(5)
-client_socket, addr = sock.accept()
-print(client_socket.recv(1024).decode("utf-8"))
-client_socket.send(b"Hello, client")
-sock.close()
