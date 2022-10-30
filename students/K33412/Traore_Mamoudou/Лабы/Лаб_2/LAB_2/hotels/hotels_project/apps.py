973e4030e23b8a4cce56a0658a231ed2c4a2427d
mamoudou-tr35 <mamoudoudiakatraore@gmail.com>
2022-10-31 00:12:49 +0300

    исправил lab

---
+++ students/K33412/Traore_Mamoudou/Лабы/Лаб_2/LAB_2/hotels/hotels_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HotelsProjectConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hotels_project"
--- students/K33412/Traore_Mamoudou/Лабы/Лаб_2/LR-2-traore/django-lab/LR2/hotel/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Client)
-admin.site.register(Hotel)
-admin.site.register(Room)
-admin.site.register(Booking)
-admin.site.register(Review)
