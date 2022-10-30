e0a4f856fcd99be29d68d6b8ef10309a53c6adf0
VadimKorobkovkii <vkorobkovskiy@gmail.com>
2022-10-30 22:23:36 +0300

    Первая практическая работа

---
+++ students/k33422/Korobkovskii_Vadim/practical_works/Pr2/Pr2_1/blogfspo/pr2_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Pr21Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pr2_1'
---
+++ students/k33422/Korobkovskii_Vadim/practical_works/Pr2/Pr2_1/blogfspo/pr2_1/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Owner(models.Model):
+    owner_id = models.IntegerField(primary_key=True)
+    surname = models.CharField(max_length=30, null=False, blank=False)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    birthday_date = models.DateField(null=True, blank=True)
+
+
+class Car(models.Model):
+    car_id = models.IntegerField(primary_key=True)
+    number = models.CharField(max_length=15, null=False, blank=False)
+    brand = models.CharField(max_length=20, null=False, blank=False)
+    model = models.CharField(max_length=20, null=False, blank=False)
+    color = models.CharField(max_length=30, null=True, blank=True)
+
+
+class Ownership(models.Model):
+    owner_car_id = models.IntegerField(primary_key=True)
+    owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    car_id = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+
+
+class DrivingLicense(models.Model):
+    license_id = models.IntegerField(primary_key=True)
+    owner_id = models.OneToOneField(Owner, on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=10, null=False, blank=False)
+    type = models.CharField(max_length=10, null=False, blank=False)
+    date_of_issue = models.DateField(null=False, blank=True)
+
+
+
+
+
+
+
+
+
