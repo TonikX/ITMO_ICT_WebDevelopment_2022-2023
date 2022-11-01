91135c002dca275d8b3438ef2436037d665fc3e6
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-01 19:22:59 +0300

    chore: register data models into admin

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/admin.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CarOwner, Car, DriversLicense, Ownership
 
-# Register your models here.
+admin.site.register(CarOwner)
+admin.site.register(Car)
+admin.site.register(DriversLicense)
+admin.site.register(Ownership)
