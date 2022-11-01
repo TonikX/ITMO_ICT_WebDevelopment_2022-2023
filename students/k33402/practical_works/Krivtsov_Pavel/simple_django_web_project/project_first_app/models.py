40d71b0026dc59562f0f4464afbae67751efa710
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-01 19:23:53 +0300

    chore: add blank=True to Null fields

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/models.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/models.py
@@ -4,25 +4,25 @@ from django.db import models
 class CarOwner(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
-    date_of_birth = models.DateTimeField(null=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
 
 
 class Car(models.Model):
     state_number = models.CharField(max_length=15)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
-    color = models.CharField(max_length=30, null=True)
+    color = models.CharField(max_length=30, blank=True, null=True)
 
 
 class DriversLicense(models.Model):
     owner_id = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
     certificate_number = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
-    date_of_issue = models.DateTimeField(null=False)
+    date_of_issue = models.DateTimeField()
 
 
 class Ownership(models.Model):
-    owner_id = models.ForeignKey(CarOwner, on_delete=models.CASCADE, null=True)
-    car_id = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
-    start_date = models.DateTimeField(null=False)
-    finish_date = models.DateTimeField(null=True)
+    owner_id = models.ForeignKey(CarOwner, on_delete=models.CASCADE, blank=True, null=True)
+    car_id = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
+    start_date = models.DateTimeField()
+    finish_date = models.DateTimeField(blank=True, null=True)
