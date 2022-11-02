1cbc3d547ca344a7079ace3e4b8f096f101ae4c6
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-02 14:42:42 +0300

    feat: add owner_details method

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/views.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/views.py
@@ -1,3 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import CarOwner
 
-# Create your views here.
+
+def owner_details(request, owner_id):
+    try:
+        owner = CarOwner.objects.get(pk=owner_id)
+    except CarOwner.DoesNotExist:
+        raise Http404("This car owner does not exist")
+
+    return render(request, 'owner.html', {'owner': owner})
