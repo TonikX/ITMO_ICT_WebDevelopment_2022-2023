a93637e704c7251ac33411e45c99abfa139ea854
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-07 14:47:53 +0300

    refactor: replace paths

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/urls.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('owner/<int:owner_id>/', views.owner_details),
     path('owners', views.owners_list),
+    path('owner/create', views.create_owner),
     path('cars', views.CarList.as_view()),
     path('car/<int:pk>/', views.CarRetrieveView.as_view()),
-    path('owner/create', views.create_owner),
     path('car/create', views.CarCreateView.as_view()),
     path('car/<int:pk>/update/', views.CarUpdateView.as_view()),
     path('car/<int:pk>/delete/', views.CarDeleteView.as_view())
