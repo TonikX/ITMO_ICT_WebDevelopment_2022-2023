dafc46694a021d0f8fa4ee4bd19da9f793346df0
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-08 21:25:02 +0300

    chore: add register and login paths

--- students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/urls.py
+++ students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.HotelList.as_view(), name="hotels"),
-    path("hotel/<int:pk>", views.HotelInfo.as_view(), name="hotel_info")
+    path("hotel/<int:pk>", views.HotelInfo.as_view(), name="hotel_info"),
+    path("login/", views.LoginUser.as_view(), name="login"),
+    path("register/", views.RegisterUser.as_view(), name="register"),
 ]
