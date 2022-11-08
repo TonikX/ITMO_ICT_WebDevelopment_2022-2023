51e66b08eea5f0f4afd1870cef892ce58b966777
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-08 21:24:39 +0300

    feat: add login form

--- students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/views.py
+++ students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -9,13 +12,27 @@ from .forms import RegisterUserForm
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = "register.html"
-    success_url = "login/"
+    success_url = reverse_lazy("login")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = "Register"
+        context['title'] = "Регистрация"
 
         return context
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Авторизация"
+
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('hotels')
 
 
 class HotelList(ListView):
