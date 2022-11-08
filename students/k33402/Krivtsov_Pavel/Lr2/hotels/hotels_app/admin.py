2099ca109ff5aaca4fc7e6a5f4eec8d66826b6da
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-08 18:02:32 +0300

    chore: register models

--- students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/admin.py
+++ students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Owner, Hotel, RoomType, Facilities, Room, Reservation, Comment
 
-# Register your models here.
+admin.site.register(Owner)
+admin.site.register(Hotel)
+admin.site.register(RoomType)
+admin.site.register(Facilities)
+admin.site.register(Room)
+admin.site.register(Reservation)
+admin.site.register(Comment)
