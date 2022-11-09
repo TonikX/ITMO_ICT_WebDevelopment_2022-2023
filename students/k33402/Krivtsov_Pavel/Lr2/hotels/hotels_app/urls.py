ef4aadec324582f9312fd9deae35e8f8adea4b7c
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-09 13:13:00 +0300

    chore: add login required condition

--- students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/urls.py
+++ students/k33402/Krivtsov_Pavel/Lr2/hotels/hotels_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path("", views.HotelList.as_view(), name="hotels"),
     path("hotel/<int:pk>", views.HotelInfo.as_view(), name="hotel_info"),
     path("room/<int:pk>", views.RoomInfo.as_view(), name="room_info"),
-    path("room/<int:pk>/reserve", views.ReserveRoom.as_view(), name="reserve"),
+    path("room/<int:pk>/reserve", login_required(views.ReserveRoom.as_view(), login_url="login"), name="reserve"),
     path("register/", views.RegisterUser.as_view(), name="register"),
     path("login/", views.LoginUser.as_view(), name="login"),
     path("logout/", views.logout_user, name="logout"),
