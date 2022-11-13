354e84eb1e48cc14845aab4a8c2aa3319738f2f5
Anastasia Konik <konik.ftl@mail.ru>
2022-11-14 00:47:06 +0300

    fix: pract 2.2 commits failed

---
+++ students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import CarOwner
+
+
+class CreateCarOwner(forms.ModelForm):
+    class Meta:
+        model = CarOwner
+        fields = ['lastname', 'first_name', 'birthday']
