356dc48468cbd1981f25ff6d74e801ae44149dd2
Anastasia Konik <konik.ftl@mail.ru>
2022-11-14 00:41:36 +0300

    feat: pract 2.2

--- students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/models.py
+++ students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/models.py
@@ -2,17 +2,24 @@ from django.db import models
 
 
 # Create your models here.
-class CarOwner(models.Model):
-    lastname = models.CharField(max_length=30, null=False)
-    first_name = models.CharField(max_length=30, null=False)
-    birthday = models.DateField()
-
-
 class Car(models.Model):
     state_number = models.CharField(max_length=15, null=False)
     make_car = models.CharField(max_length=20, null=False)
     model_car = models.CharField(max_length=20, null=False)
     colour = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.id.__str__()
+
+
+class CarOwner(models.Model):
+    lastname = models.CharField(max_length=30, null=False)
+    first_name = models.CharField(max_length=30, null=False)
+    birthday = models.DateField()
+    cars = models.ManyToManyField(Car, through='Ownership')
+
+    def __str__(self):
+        return self.id.__str__()
 
 
 class Ownership(models.Model):
@@ -20,6 +27,9 @@ class Ownership(models.Model):
     id_car = models.ForeignKey(Car, on_delete=models.CASCADE)
     start_date = models.DateField()
     expiration_date = models.DateField(null=True)
+
+    def __str__(self):
+        return "{}_{}".format(self.id_owner.__str__(), self.id_car.__str__())
 
 
 class DriverLicense(models.Model):
--- students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/urls.py
+++ students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('owner/<int:id>', views.info_about_car_owner)
+    path('owner/<int:id>', info_about_car_owner),
+    path('owners/', owners),
+    path('cars/', CarsList.as_view()),
+    path('car/<int:pk>', CarById.as_view()),
+    path('create_car_owner/', create_car_owner),
+    path('create_car/', CarCreate.as_view()),
+    path('update_car/<int:pk>', CarUpdate.as_view()),
+    path('delete_car/<int:pk>', CarDelete.as_view()),
 ]
