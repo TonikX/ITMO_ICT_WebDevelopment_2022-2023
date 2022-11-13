a7727a5b2b88a6c33e75f2332d7cde093ad9bcde
IYunet <lbvfzx@gmail.com>
2022-11-13 13:38:49 +0300

    Сделал отчет по 3.1

--- students/k33401/practical_works/Kozlov Ivan/pr_3/blogfspo/blog/models.py
+++ students/k33401/practical_works/Kozlov Ivan/pr_3/blogfspo/blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Transport_owner(models.Model):
     id_owner = models.IntegerField(primary_key=True)
     last_name = models.CharField(max_length=30, null=False)
