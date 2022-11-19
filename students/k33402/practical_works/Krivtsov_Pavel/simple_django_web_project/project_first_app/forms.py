f417276c95d207556b9b5d37053e35abc898895e
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-19 16:53:36 +0300

    feat: make CarOwner AuthUser

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/forms.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/forms.py
@@ -1,13 +1,30 @@
 from django import forms
 from .models import CarOwner
+from django.contrib.auth.forms import UserCreationForm
 
 
-class CreateOwnerForm(forms.ModelForm):
+class CreateOwnerForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'from-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'from-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'from-input'}))
+    first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'from-input'}))
+    second_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'from-input'}))
+    date_of_birth = forms.DateTimeField(label='Дата рождения', widget=forms.DateInput(attrs={'type': 'date'}))
+    passport_number = forms.CharField(label='Номер паспорта', widget=forms.TextInput(attrs={'class': 'from-input'}))
+    nationality = forms.CharField(label='Национальность', widget=forms.TextInput(attrs={'class': 'from-input'}))
+    address = forms.TextInput()
+
     class Meta:
         model = CarOwner
 
         fields = [
+            "username",
+            "password1",
+            "password2",
             "first_name",
             "second_name",
-            "date_of_birth"
+            "date_of_birth",
+            "passport_number",
+            "nationality",
+            "address"
         ]
--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/simple_django_web_project/settings.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/simple_django_web_project/settings.py
@@ -110,3 +110,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# User model
+
+AUTH_USER_MODEL = 'project_first_app.CarOwner'
