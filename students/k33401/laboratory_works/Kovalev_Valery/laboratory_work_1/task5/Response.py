51dbefe0a916cf9a8c19988f3a40626a13b771d5
snyderlt <103443107+snyderlt@users.noreply.github.com>
2022-11-21 20:23:24 +0300

    Delete students/k33401/laboratory_works/Kovalev_Valery directory

--- students/k33401/laboratory_works/Kovalev_Valery/laboratory_work_1/task5/Response.py
+++
@@ -1,6 +0,0 @@
-class Response:
-    def __init__(self, status, reason, headers=None, body=None):
-        self.status = status
-        self.reason = reason
-        self.headers = headers
-        self.body = body
