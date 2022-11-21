38d7349ef6fdf78219c70ffe966617d6c2b00e58
snyderlt <103443107+snyderlt@users.noreply.github.com>
2022-11-21 20:06:16 +0300

    Delete 3/djangoApp directory

--- 3/djangoApp/First_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Owner_Model
-from .models import Car_Model
-
-# Register your models here.
-
-admin.site.register(Owner_Model)
-admin.site.register(Car_Model)
