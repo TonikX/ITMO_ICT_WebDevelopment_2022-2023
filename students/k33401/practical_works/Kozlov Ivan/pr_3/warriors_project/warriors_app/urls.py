5658819d2069d4364f246ad514d827faed5310e2
IYunet <lbvfzx@gmail.com>
2022-11-22 19:23:37 +0300

    добавил swagger, выполнил практику 3.3

--- students/k33401/practical_works/Kozlov Ivan/pr_3/warriors_project/warriors_app/urls.py
+++ students/k33401/practical_works/Kozlov Ivan/pr_3/warriors_project/warriors_app/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 from .views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+      title="API",
+      default_version='v2',
+      description="Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="my@n.ru"),
+      license=openapi.License(name="BSD License"),
+   ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 app_name = "warriors_app"
 
@@ -12,4 +28,9 @@ urlpatterns = [
     path('get_warrios_by_id/<int:pk>/', WarriorView.as_view()),
     path('del_warrios_by_id/<int:pk>/', DelWarriorView.as_view()),
     path('update_warrios_by_id/<int:pk>/', UpdateWarriors.as_view()),
+    path('doc/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+
 ]
+
+
--- students/k33401/practical_works/Kozlov Ivan/pr_3/warriors_project/warriors_project/settings.py
+++ students/k33401/practical_works/Kozlov Ivan/pr_3/warriors_project/warriors_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'warriors_app',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
