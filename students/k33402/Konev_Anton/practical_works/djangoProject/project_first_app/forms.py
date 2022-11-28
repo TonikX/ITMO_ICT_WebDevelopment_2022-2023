079902d0f8d8d4ecb2a63f66435220d35a5fae7d
Ant <taco.v@yandex.ru>
2022-11-28 18:46:59 +0300

    fix directory of PW 2

--- students/k33402/Konev_Anton/practical_works/djangoProject/project_first_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import CarOwner
-
-
-class CreateOwner(forms.ModelForm):
-    class Meta:
-        model = CarOwner
-        fields = ['owner_id', 'last_name', 'first_name', 'birth_date', 'passport', 'address', 'nationality', 'username']
