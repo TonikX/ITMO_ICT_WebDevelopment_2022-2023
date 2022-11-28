1118f5a155151d325a659e058ccac288fcd7790f
Ant <taco.v@yandex.ru>
2022-11-28 18:47:52 +0300

    add prac 3.1

--- students/k33402/Konev_Anton/practical_works/Konev_Anton/pw_3/blogfspo/project_first_app/models.py
+++ students/k33402/Konev_Anton/practical_works/Konev_Anton/pw_3/blogfspo/project_first_app/models.py
@@ -3,19 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class CarOwner(AbstractUser):
-    owner_id = models.IntegerField(primary_key=True)
+class Car_owner(models.Model):
+    id_owner = models.IntegerField(primary_key=True)
     last_name = models.CharField(max_length=30, null=False)
     first_name = models.CharField(max_length=30, null=False)
     birth_date = models.DateField(null=True)
-    passport = models.IntegerField(null=True)
-    address = models.CharField(max_length=40, null=False, blank=True)
-    nationality = models.CharField(max_length=20, null=False, blank=True)
-    username = models.CharField(max_length=30, null=False, unique=True)
 
 
 class Car(models.Model):
-    car_id = models.IntegerField(primary_key=True)
+    id_car = models.IntegerField(primary_key=True)
     number_plate = models.CharField(max_length=15, null=False)
     brand = models.CharField(max_length=20, null=False)
     model = models.CharField(max_length=20, null=False)
@@ -23,16 +19,16 @@ class Car(models.Model):
 
 
 class Ownership(models.Model):
-    car_owner_id = models.IntegerField(primary_key=True)
-    owner_id = models.ForeignKey("CarOwner", on_delete=models.CASCADE)
-    car_id = models.ForeignKey("Car", on_delete=models.CASCADE)
+    id_owner_car = models.IntegerField(primary_key=True)
+    id_owner = models.ForeignKey(Car_owner, on_delete=models.CASCADE)
+    id_car = models.ForeignKey(Car, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField(null=True)
 
 
-class DriverLicense(models.Model):
-    license_id = models.IntegerField(primary_key=True)
-    owner_id = models.ForeignKey("CarOwner", on_delete=models.CASCADE)
+class Driver_license(models.Model):
+    id_license = models.IntegerField(primary_key=True)
+    id_owner = models.ForeignKey(Car_owner, on_delete=models.CASCADE)
     license_number = models.CharField(max_length=10, null=False)
     type = models.CharField(max_length=10, null=False)
     issue_date = models.DateField()
