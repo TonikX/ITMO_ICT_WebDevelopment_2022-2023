22260c34fed566f164013b219547d74182cfee9d
Sveta-ops <s.kulagina2002@gmail.com>
2022-11-30 03:12:49 +0300

    the pr3 is done

--- students/K33412/Kulagina Svetlana/Lr2/tourfirm/tourism/forms.py
+++ students/K33412/Kulagina Svetlana/Lr2/tourfirm/tourism/forms.py
@@ -7,6 +7,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
+
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
