d99264ba29646fe2f4975f66b8fa483d21078eab
snyderlt <103443107+snyderlt@users.noreply.github.com>
2022-11-30 15:07:59 +0300

    Delete students/D33101/Liang_Tao/Lr3 directory

--- students/D33101/Liang_Tao/Lr3/3.1/lab3/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Driver)
-admin.site.register(Car)
-admin.site.register(License)
-admin.site.register(Owner)
