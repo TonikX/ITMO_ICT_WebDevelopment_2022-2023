70c3eec5b65e440890a86f6c9d69c38214d9d389
Илья Селезнев <46225413+SeLeZniOFF@users.noreply.github.com>
2022-12-08 19:17:56 +0300

    Delete students/K33412/Laboratory_works/laboratory_works_2/races_project directory

--- students/K33412/Laboratory_works/laboratory_works_2/races_project/races/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Driver)
-admin.site.register(Car)
-admin.site.register(Race)
-admin.site.register(Registration)
-admin.site.register(Comment)
