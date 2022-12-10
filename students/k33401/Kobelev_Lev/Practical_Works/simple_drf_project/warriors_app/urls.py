111415590409da3de28ac0cbcf528cbfae472cf2
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2022-12-10 16:16:51 +0300

    :sparkles: [add] swagger

--- students/k33401/Kobelev_Lev/Practical_Works/simple_drf_project/warriors_app/urls.py
+++ students/k33401/Kobelev_Lev/Practical_Works/simple_drf_project/warriors_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('warriors/<int:pk>/', WarriorAPIView.as_view()),
     path('warriors/<int:pk>/update/', WarriorAPIUpdate.as_view()),
     path('warriors/<int:pk>/delete/', WarriorAPIDelete.as_view()),
-    path('jobs', JobsListAPIView.as_view()),
+    path('jobs/', JobsListAPIView.as_view()),
     path('job/create/', JobCreateAPIView.as_view()),
     path('skills/', SkillListAPIView.as_view()),
     path('skill/create/', SkillCreateAPIView.as_view()),
