f4b9e2ec8407e20bd92e8030d74b9cdc13662a9e
rybkin <github@rybkin.me>
2022-12-15 18:43:47 +0300

    updated admin interface everywhere

--- lab2/apps/groups/admin.py
+++ lab2/apps/groups/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from apps.groups.models import Group
 
-admin.site.register(Group)
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'teacher')
+    list_filter = ('subject', 'teacher')
+
+
+admin.site.register(Group, GroupAdmin)
--- lab2/apps/homeworks/admin.py
+++ lab2/apps/homeworks/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from apps.homeworks.models import Homework
 
-admin.site.register(Homework)
+
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'group', 'date_start', 'date_end')
+    list_filter = ('group',)
+
+
+admin.site.register(Homework, HomeworkAdmin)
