8c9fe7e65f876213f810df11f5e181c8d2fb8e3b
rybkin <github@rybkin.me>
2022-12-15 16:17:51 +0300

    added admin interfaces

--- lab2/apps/homeworks/admin.py
+++ lab2/apps/homeworks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.homeworks.models import Homework
+
+admin.site.register(Homework)
--- lab2/apps/submissions/admin.py
+++ lab2/apps/submissions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.submissions.models import Submission
+
+admin.site.register(Submission)
