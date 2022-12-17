a2bd6b2c98f184f76bda36300e8f31e62923b08c
Anastasia Konik <konik.ftl@mail.ru>
2022-12-17 15:04:58 +0300

    feat: add pract 3.1

--- students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/models.py
+++ students/k33412/practical_works/Anastasia_Konik/simple_django_web_project/project_first_app/models.py
@@ -4,17 +4,19 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Car(models.Model):
+    car_id = models.IntegerField(primary_key=True)
     state_number = models.CharField(max_length=15, null=False)
     make_car = models.CharField(max_length=20, null=False)
     model_car = models.CharField(max_length=20, null=False)
     colour = models.CharField(max_length=30, null=True)
 
-    def __str__(self):
-        return self.id.__str__()
+    # def __str__(self):
+    #     return self.car_id.__str__()
 
 
 class CarOwner(AbstractUser):
-    lastname = models.CharField(max_length=30, null=False)
+    owner_id = models.IntegerField(primary_key=True)
+    last_name = models.CharField(max_length=30, null=False)
     first_name = models.CharField(max_length=30, null=False)
     birthday = models.DateField(null=True)
     passport = models.CharField(max_length=12, null=True, blank=True)
@@ -23,21 +25,23 @@ class CarOwner(AbstractUser):
     cars = models.ManyToManyField(Car, through='Ownership')
 
     def __str__(self):
-        return self.id.__str__()
+        return self.owner_id.__str__()
 
 
 class Ownership(models.Model):
-    id_owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
-    id_car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    owner_car_id = models.IntegerField(primary_key=True)
+    owner_id = models.ForeignKey(CarOwner, on_delete=models.CASCADE, related_name='owner')
+    car_id = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car')
     start_date = models.DateField()
     expiration_date = models.DateField(null=True)
 
     def __str__(self):
-        return "{}_{}".format(self.id_owner.__str__(), self.id_car.__str__())
+        return "{}_{}".format(self.owner_id.__str__(), self.car_id.__str__())
 
 
 class DriverLicense(models.Model):
-    id_owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
+    license_id = models.IntegerField(primary_key=True)
+    owner_id = models.ForeignKey(CarOwner, on_delete=models.CASCADE, related_name='carOwner')
     license_number = models.CharField(max_length=10, null=False)
     type = models.CharField(max_length=10, null=False)
     date_of_license = models.DateField()
