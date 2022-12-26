29b5df32e551b0b38dd006b7c56a33440a2f50d8
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2022-12-26 10:10:38 +0300

    :hammer: [fix] model on game system serializer

--- students/k33401/Kobelev_Lev/LR3/scenarios_app/serializers.py
+++ students/k33401/Kobelev_Lev/LR3/scenarios_app/serializers.py
@@ -10,7 +10,7 @@ class TagSerializer(serializers.ModelSerializer):
 
 class GameSystemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tag
+        model = GameSystem
         fields = '__all__'
 
 
