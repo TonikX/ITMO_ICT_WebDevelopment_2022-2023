f8616fa68240fce036f393c79ca7316305369bad
Olga Lipina <neon_alfa@mail.ru>
2022-12-28 17:20:08 +0300

    login and lohout jwt done

---
+++ students/k33421/laboratory_works/Lipina_Olga/laboratory_work_4/frontend/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Библиотека</h1>
+    <button @click="goLogin">Вход</button>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    goLogin() {
+      this.$router.push({name: "login"})
+    }
+  }
+}
+</script>
+
