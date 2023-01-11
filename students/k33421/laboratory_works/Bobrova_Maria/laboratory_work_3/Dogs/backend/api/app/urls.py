f887e324e9bc945fcc36d0694ccd841195a8a915
Bobrova Maria <70529271+Martinumer@users.noreply.github.com>
2023-01-11 15:08:37 +0300

    Delete students/k33421/laboratory_works/Bobrova_Maria/laboratory_work_3 directory

--- students/k33421/laboratory_works/Bobrova_Maria/laboratory_work_3/Dogs/backend/api/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('exhibition/', AllExhibition.as_view()),
-]
--- students/k33421/laboratory_works/Bobrova_Maria/laboratory_work_3/Dogs/backend/api/exhibition/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', AllExhibition.as_view()),
-]
