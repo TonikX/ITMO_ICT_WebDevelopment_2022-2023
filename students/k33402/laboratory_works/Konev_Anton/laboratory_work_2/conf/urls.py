eb34120d8121e9455fdf0bcfc4f8e2fcbc04c593
Anton Konev <taco.v@yandex.ru>
2023-01-11 21:20:21 +0500

    [fix] fix laboratory directory name

--- students/k33402/laboratory_works/Konev_Anton/laboratory_work_2/conf/urls.py
+++ students/k33402/laboratory_works/Konev_Anton/laboratory_work_2/conf/urls.py
@@ -1,4 +1,4 @@
-"""LW_2 URL Configuration
+"""laboratory_work_2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- students/k33402/laboratory_works/Konev_Anton/laboratory_work_2/manage.py
+++ students/k33402/laboratory_works/Konev_Anton/laboratory_work_2/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LW_2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laboratory_work_2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
