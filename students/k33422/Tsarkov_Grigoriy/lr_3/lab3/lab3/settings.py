e057acc7a3a6025fffe5976c6944a59203456ebf
Gregorie <grishagti@yandex.ru>
2023-01-11 13:29:45 +0300

    Добавлены ПР2 и ПР3

--- students/k33422/Tsarkov_Grigoriy/lr_3/lab3/lab3/settings.py
+++ students/k33422/Tsarkov_Grigoriy/lr_3/lab3/lab3/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +56,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "lab3.urls"
 
