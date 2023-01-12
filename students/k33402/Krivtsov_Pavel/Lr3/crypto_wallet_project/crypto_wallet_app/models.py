67d2b97f5bcff312ef49bcb906b45bc2b6ccb6b6
Krivtsov Pavel <pkrivtsov6@gmail.com>
2023-01-12 23:50:45 +0300

    refactor: get rid of warnings

--- students/k33402/Krivtsov_Pavel/Lr3/crypto_wallet_project/crypto_wallet_app/models.py
+++ students/k33402/Krivtsov_Pavel/Lr3/crypto_wallet_project/crypto_wallet_app/models.py
@@ -9,13 +9,13 @@ class Currency(models.Model):
     users = models.ManyToManyField(User, verbose_name="Владельцы", through="Ownership")
     name = models.CharField(verbose_name="Название", max_length=30, unique=True)
     abbreviation = models.CharField(verbose_name="Сокращение", max_length=5, unique=True)
-    price = models.DecimalField(verbose_name="Цена за единицу")
+    price = models.DecimalField(verbose_name="Цена за единицу", max_digits=19, decimal_places=10)
     count = models.IntegerField(verbose_name="Количество в обращении")
     daily_changes = models.FloatField(verbose_name="Изменение цены за день (%)")
     weekly_changes = models.FloatField(verbose_name="Изменение цены за неделю (%)")
-    date_added = models.DateField(verbose_name="Дата появления", auto_now=True)
-    coinlib_id = models.PositiveIntegerField(verbose_name="Id на сайте Coinlib.io", max_length=5)
-    image = models.CharField(verbose_name="Ссылка на изображение")
+    date_added = models.DateTimeField(verbose_name="Дата появления", auto_now=True)
+    coinlib_id = models.PositiveIntegerField(verbose_name="Id на сайте Coinlib.io")
+    image = models.CharField(verbose_name="Ссылка на изображение", max_length=1000)
 
     def __str__(self):
         return f"{self.name} ({self.abbreviation})"
@@ -42,7 +42,7 @@ class Discussion(models.Model):
     tags = models.ManyToManyField(Tag, verbose_name="Тэги")
     title = models.CharField(verbose_name="Тема обсуждения", max_length=80)
     description = models.TextField(verbose_name="Описание")
-    date_added = models.DateField(verbose_name="Дата публикации", auto_now=True)
+    date_added = models.DateTimeField(verbose_name="Дата публикации", auto_now=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.date_added}: {self.title}"
@@ -64,9 +64,9 @@ class Transaction(models.Model):
         ('s', 'sell')
     )
 
-    user_id = models.IntegerField()
-    currency_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
     transaction_type = models.CharField(max_length=1, choices=TR_TYPE)
-    count = models.IntegerField(max_length=10000)
-    transaction_amount = models.DecimalField()
+    count = models.IntegerField()
+    transaction_amount = models.DecimalField(max_digits=40, decimal_places=20)
     transaction_time = models.DateTimeField(auto_now=True)
