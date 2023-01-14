78dc2ba45a5d7d27afcce6ae6f0a602dc4347783
EgorChernov3140 <egorchernov3140@gmail.com>
2023-01-14 23:10:49 +0300

    Lr3: change ACCESS_TOKEN_LIFETIME

--- students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/hotel/settings.py
+++ students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/hotel/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'auth.User'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
@@ -187,7 +187,7 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=30),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
