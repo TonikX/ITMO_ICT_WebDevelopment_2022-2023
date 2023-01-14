3e9eeea3e1588571aa2209ec63e99f5a206341a4
EgorChernov3140 <egorchernov3140@gmail.com>
2023-01-14 19:52:06 +0300

    Lr3: fix

--- students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/account_app/views.py
+++ students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/account_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
-
 from django.contrib.auth.models import User
+
 from .serializers import UserSerializer
 from .permissions import IsOwner
 
