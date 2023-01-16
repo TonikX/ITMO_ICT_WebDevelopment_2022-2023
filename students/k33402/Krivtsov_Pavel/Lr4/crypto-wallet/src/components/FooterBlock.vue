c063c2b3ce277f5592dd4cbf08c0ad755bc6cf48
Krivtsov Pavel <pkrivtsov6@gmail.com>
2023-01-16 22:40:14 +0300

    chore: delete bottom binding

--- students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/components/FooterBlock.vue
+++ students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/components/FooterBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-navbar fixed-bottom">
+  <footer class="bg-navbar">
     <div class="container">
       <p class="col-md-4 mb-0 color-main py-3">&copy; 2022 Crypto Wallet, Inc</p>
     </div>
