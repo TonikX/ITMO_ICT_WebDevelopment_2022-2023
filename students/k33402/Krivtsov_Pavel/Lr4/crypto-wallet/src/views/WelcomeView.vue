7d46e9b03d9a73d2c63a0b9cdfe0ff9013f0c88e
Krivtsov Pavel <pkrivtsov6@gmail.com>
2023-01-16 23:42:09 +0300

    feat: add currencies list

--- students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/views/WelcomeView.vue
+++ students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/views/WelcomeView.vue
@@ -3,22 +3,62 @@
   <main>
     <section class="min-vh-100">
       <WelcomeSection/>
+      <section>
+        <div class="container py-5">
+          <div class="d-flex justify-content-center pb-5">
+            <h2>Купить криптовалюту</h2>
+          </div>
+
+          <CurrenciesList :currencies="currencies" />
+
+          <div class="d-flex justify-content-center pt-4">
+            <div tabindex="0">
+              <router-link to="/markets" class="link-side">
+                Посмотреть больше
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   </main>
   <FooterBlock/>
 </template>
 
 <script>
+import $ from "jquery"
+
 import HeaderNavBar from "../components/HeaderNavBar.vue";
 import FooterBlock from "../components/FooterBlock.vue";
 import WelcomeSection from "../components/WelcomeSection.vue";
+import CurrenciesList from "../components/CurrenciesList.vue";
 
 export default {
   name: "WelcomeView",
   components: {
     HeaderNavBar,
     FooterBlock,
-    WelcomeSection
+    WelcomeSection,
+    CurrenciesList
+  },
+  data() {
+    return {
+      currencies: []
+    }
+  },
+  created() {
+    this.loadPopularCurrencies()
+  },
+  methods: {
+    loadPopularCurrencies() {
+      $.ajax({
+        url: "http://127.0.0.1:8000/currencies/popular/",
+        type: "GET",
+        success: (response) => {
+          this.currencies = response
+        }
+      })
+    }
   }
 }
 </script>
