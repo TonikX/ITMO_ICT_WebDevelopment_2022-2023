9b84cf5edb03e31c8a67a5e44fb33e32db239020
Nikolas <timofeevnik41@gmail.com>
2023-01-16 22:04:17 +0300

    cleanup (removed lab1/2 from PR)

--- students/k33402/Timofeev_Nikolay/LR2/megaultrabooking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from megaultrabooking.models import Hotel, Room, Review, User, Booking
-
-admin.site.register(Hotel)
-admin.site.register(Room)
-admin.site.register(Review)
-admin.site.register(User)
-admin.site.register(Booking)
--- students/k33402/Timofeev_Nikolay/LR2/megaultrabooking/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MegaultrabookingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "megaultrabooking"
