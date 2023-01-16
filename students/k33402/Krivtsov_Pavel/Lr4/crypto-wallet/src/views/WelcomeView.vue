3a074fa5579fcbb5112463042f30959634cc8841
Krivtsov Pavel <pkrivtsov6@gmail.com>
2023-01-16 22:40:56 +0300

    feat: add welcome section

--- students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/views/WelcomeView.vue
+++ students/k33402/Krivtsov_Pavel/Lr4/crypto-wallet/src/views/WelcomeView.vue
@@ -1,17 +1,24 @@
 <template>
   <HeaderNavBar/>
+  <main>
+    <section class="min-vh-100">
+      <WelcomeSection/>
+    </section>
+  </main>
   <FooterBlock/>
 </template>
 
 <script>
 import HeaderNavBar from "../components/HeaderNavBar.vue";
 import FooterBlock from "../components/FooterBlock.vue";
+import WelcomeSection from "../components/WelcomeSection.vue";
 
 export default {
   name: "WelcomeView",
   components: {
     HeaderNavBar,
-    FooterBlock
+    FooterBlock,
+    WelcomeSection
   }
 }
 </script>
