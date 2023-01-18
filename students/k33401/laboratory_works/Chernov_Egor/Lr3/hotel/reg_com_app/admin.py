b839148b919ed970f3d990ac81119d3ca82ebf44
EgorChernov3140 <egorchernov3140@gmail.com>
2023-01-19 01:46:26 +0300

    Lr3: refactor

---
+++ students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/reg_com_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Registration, Comment
+
+
+admin.site.register(Registration)
+admin.site.register(Comment)
---
+++ students/k33401/laboratory_works/Chernov_Egor/Lr3/hotel/reg_com_app/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+
+from .models import Registration, Comment
+from .serializers import RegistrationSerializer, WriteRegistrationSerializer, CommentSerializer
+from .permisions import IsOwner
+
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser | IsOwner, ]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Registration.objects.all()
+        return Registration.objects.filter(user_reg=user)
+
+    def get_serializer_class(self):
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return WriteRegistrationSerializer
+        return RegistrationSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'retrieve'):
+            self.permission_classes = [AllowAny, ]
+        elif self.action in ('create', 'update', 'destroy'):
+            self.permission_classes = [IsAdminUser | IsOwner, ]
+        return [permission() for permission in self.permission_classes]
