383c609a35fd5feaa8887cbf0772524060d41fd1
Oceesay100 <74785427+Oceesay100@users.noreply.github.com>
2023-01-26 04:27:38 +0300

    this is labs03

---
+++ students/k33412/laboratory_works/dogs_front/src/components/ParticipantsList.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <v-card
+      elevation="2"
+      outlined
+      class="my-2"
+    >
+    <v-card-text>
+ <div  v-for="participant in participants" :key="participant.id"> 
+   <div><strong>nickname:</strong> {{ participant.name }}</div>
+   <div><strong>breed:</strong> {{ participant.breed }}</div>
+   <div><strong>age:</strong> {{ participant.age }}</div>
+   <div><strong>family:</strong> {{ participant.family }}</div>
+   <div><strong>owner data:</strong> {{ participant.owner_data }}</div>
+   <div><strong>club:</strong> {{ participant.club }}</div><br>
+ </div>
+    </v-card-text>
+  </v-card>
+</div>
+</template>
+
+<script>
+export default {
+ props: { 
+   participants: {
+     type: Array,
+     required: true
+   }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ students/k33412/laboratory_works/dogs_front/src/components/ParticipationList.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <v-card
+      elevation="2"
+      outlined
+      class="my-2"
+    >
+  <v-card-text>  
+ <div  v-for="participation in participations" :key="participation.id"> <!-- v-for - директива для отображения списка элементов на основе массива. -->
+   <div><strong>Medal:</strong> {{ participation.medal }}</div>
+   <div><strong>Vaccination:</strong> {{ participation.vaccinated }}</div>
+   <div><strong>Dismissed:</strong> {{ participation.dismissed }}</div>
+   <div><strong>Grade:</strong> {{ participation.final_grade }}</div>
+   <div><strong>participant:</strong> {{ participation.participant }}</div>
+   <div><strong>Ring:</strong> {{ participation.rings }}</div><br>
+ </div>
+  </v-card-text>
+    </v-card>
+</div>
+</template>
+
+<script>
+export default {
+ props: { // «Props» -- это специальное ключевое слово, обозначающее свойства . Его можно зарегистрировать в компоненте для передачи данных от родительского компонента к одному из его дочерних компонентов.
+   participations: {
+     type: Array,
+     required: true
+   }
+ }
+}
+</script>
+
+<style scoped>
+/* eslint-disable */
+</style>
