6eede85b4c292322e8f3a29edea5ce239b9a4b46
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-02-07 23:19:05 +0300

    fix

--- students/k33401/Reingeverts_Vadim/Lr3/Lr/Librarian/run.sh
+++ students/k33401/Reingeverts_Vadim/Lr3/Lr/Librarian/run.sh
@@ -7,11 +7,7 @@ cd "$parent_path"
 if [ -z "$1" ] # https://stackoverflow.com/a/19486205
 then
     # Migrates and runs dev server
-    source ../../../.web-dev-env/Scripts/activate && mkdocs serve -a localhost:8811 &
     source ../../../.web-dev-env/Scripts/activate && python manage.py makemigrations && python manage.py migrate && python manage.py runserver
-elif [ $1 = "docs" ]
-then
-    source ../../../.web-dev-env/Scripts/activate && mkdocs serve -a localhost:8811
 else
     source ../../../.web-dev-env/Scripts/activate && python manage.py "$@"
 fi
