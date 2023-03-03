7e815b0d8b052bfff8e0260870dae09b3a5e892f
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-03-03 16:31:12 +0300

    init: copy over Lr3

---
+++ students/k33401/Reingeverts_Vadim/Lr4/Lr/Librarian/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+# Makes it so it doesn't matter from which directory script is called
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P ) # https://stackoverflow.com/a/24112741
+cd "$parent_path"
+
+# If there are arguments provided, pass them to the `manage.py`
+if [ -z "$1" ] # https://stackoverflow.com/a/19486205
+then
+    # Migrates and runs dev server
+    source ../../../.web-dev-env/Scripts/activate && python manage.py makemigrations && python manage.py migrate && python manage.py runserver
+else
+    source ../../../.web-dev-env/Scripts/activate && python manage.py "$@"
+fi
