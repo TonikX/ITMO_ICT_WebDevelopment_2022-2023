97478bf0ff84612567dc358238c390499c53bd29
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-03-06 18:13:43 +0300

    feat: add logout button

--- students/k33401/Reingeverts_Vadim/Lr4/Lr/Librarian/frontend/static/src/components/User.jsx
+++ students/k33401/Reingeverts_Vadim/Lr4/Lr/Librarian/frontend/static/src/components/User.jsx
@@ -1,6 +1,56 @@
 import React from "react";
+import {
+    UnstyledButton,
+    Group,
+    Avatar,
+    Box,
+    useMantineTheme,
+    rem,
+    Popover,
+    Text,
+    Button,
+    MediaQuery,
+} from "@mantine/core";
 import { IconChevronRight, IconChevronLeft } from "@tabler/icons-react";
-import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme, rem } from "@mantine/core";
+
+const LogoutButton = () => <Button color="red">Log Out</Button>;
+
+const getUserContainerSx = (theme) => {
+    return {
+        display: "block",
+        width: "100%",
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+        "&:hover": {
+            backgroundColor:
+                theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+    };
+};
+
+const UserContent = ({ right }) => {
+    const theme = useMantineTheme();
+
+    return (
+        <Group spacing="xs">
+            <Avatar
+                src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
+                radius="xl"
+            />
+            <Box sx={{ flex: 1 }}>
+                <Text size="sm" weight={500}>
+                    Amy Horsefighter
+                </Text>
+                <Text color="dimmed" size="xs">
+                    ahorsefighter
+                </Text>
+            </Box>
+            {right}
+        </Group>
+    );
+};
 
 const User = () => {
     const theme = useMantineTheme();
@@ -14,43 +64,23 @@ const User = () => {
                 }`,
             }}
         >
-            <UnstyledButton
-                sx={{
-                    display: "block",
-                    width: "100%",
-                    padding: theme.spacing.xs,
-                    borderRadius: theme.radius.sm,
-                    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-
-                    "&:hover": {
-                        backgroundColor:
-                            theme.colorScheme === "dark"
-                                ? theme.colors.dark[6]
-                                : theme.colors.gray[0],
-                    },
-                }}
-            >
-                <Group>
-                    <Avatar
-                        src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-                        radius="xl"
-                    />
-                    <Box sx={{ flex: 1 }}>
-                        <Text size="sm" weight={500}>
-                            Amy Horsefighter
-                        </Text>
-                        <Text color="dimmed" size="xs">
-                            ahorsefighter
-                        </Text>
-                    </Box>
-
-                    {theme.dir === "ltr" ? (
-                        <IconChevronRight size={rem(18)} />
-                    ) : (
-                        <IconChevronLeft size={rem(18)} />
-                    )}
-                </Group>
-            </UnstyledButton>
+            <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+                <Popover position="right" shadow="md" radius="sm" offset={8}>
+                    <Popover.Target>
+                        <UnstyledButton sx={getUserContainerSx(theme)}>
+                            <UserContent right={<IconChevronRight size={rem(18)} />} />
+                        </UnstyledButton>
+                    </Popover.Target>
+                    <Popover.Dropdown>
+                        <LogoutButton />
+                    </Popover.Dropdown>
+                </Popover>
+            </MediaQuery>
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Box sx={getUserContainerSx(theme)}>
+                    <UserContent right={<LogoutButton />} />
+                </Box>
+            </MediaQuery>
         </Box>
     );
 };
